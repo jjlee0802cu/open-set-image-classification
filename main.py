6d39112f3b8b1cca5777236dd9403bfb5b733964
jjlee0802cu <jjl2245@columbia.edu>
2023-02-16 12:17:44 -0500

    Add baseline model for fashion MNIST

--- main.py
+++ main.py
@@ -17,10 +17,16 @@ fashion_mnist = keras.datasets.fashion_mnist
 
 # Leave out 5 of the classes during training. Classes 5-9 are designated as "unkown"
 """
+Original split:
+    Train: 60000 samples
+        0-9 have 6000 samples
+    Test: 10000 samples
+        0-9 have 1000 samples
+
 Resulting split:
-    Train:
+    Train: 30000 samples
         0-4 have 6000 samples
-    Test:
+    Test: 40000 samples
         0-4 have 1000 samples
         5-9 have 7000 samples
 
@@ -43,6 +49,25 @@ test_y = np.concatenate((test_y, np.array(unknown_y)), axis=0)
 print(train_x.shape, train_y.shape)
 print(test_x.shape, test_y.shape)
 
+
+
+
+
+
+# normalization
+train_x = train_x / 255.0
+test_x = test_x / 255.0
+
+# this one uses cnn: https://machinelearningmastery.com/how-to-develop-a-cnn-from-scratch-for-fashion-mnist-clothing-classification/
+
+# this is baseline model from https://www.kaggle.com/code/arunkumarramanan/awesome-cv-with-fashion-mnist-classification/notebook
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_x, train_y, epochs=5)
 
 
 
