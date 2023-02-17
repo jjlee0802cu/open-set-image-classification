297024d93f00289aa3782ba582f569bd306f8c38
jjlee0802cu <jjl2245@columbia.edu>
2023-02-17 16:41:55 -0500

    Computed TPR, TNR, FPR, FNR and add plots for the rates and in-set accuracies

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from sklearn.metrics import DetCurveDisplay
-
+import matplotlib.pyplot as plt
 
 # Get Fashion MNIST dataset
 '''
@@ -58,7 +58,7 @@ test_y = np.concatenate((test_y, np.array(unknown_y)), axis=0)
 # Change labels of unkown classes to be negative numbers
 for i in range(len(test_y)):
     if test_y[i] >= 5:
-        test_y[i] = -1
+        test_y[i] = -test_y[i] + 4
 """
 New split:
     Train: 30000 samples
@@ -73,7 +73,11 @@ New split:
         2: 1000 samples
         3: 1000 samples
         4: 1000 samples
-        -1: 35000 samples
+        -1: 7000 samples
+        -2: 7000 samples
+        -3: 7000 samples
+        -4: 7000 samples
+        -5: 7000 samples
 """
 print()
 
@@ -118,21 +122,81 @@ def apply_threshold(threshold, predictions):
             threshold_predictions.append(-1)
     return threshold_predictions
 
+def get_accuracy(dictionary):
+    try:
+        return dictionary['correct']/dictionary['total']
+    except:
+        return 1
+
 predictions = model.predict(test_x)
-threshold_predictions = apply_threshold(0.9, predictions)
 
 
-correct = 0
-c,t = 0,0
-for i in range(len(threshold_predictions)):
-    if sign(threshold_predictions[i]) == sign(test_y[i]):
-        correct += 1
-    if sign(threshold_predictions[i]) > 0 and sign(test_y[i]) > 0:
-        t += 1
-        if threshold_predictions[i] == test_y[i]:
-            c += 1
-print(correct/len(threshold_predictions))
-print(c/t)
+tpr_list = []
+fpr_list = []
+fnr_list = []
+tnr_list = []
+id_accuracies = []
+threshold_to_test = np.linspace(0, 1, 200)
+for threshold in threshold_to_test:
+    print("threshold:", threshold)
+    threshold_predictions = apply_threshold(threshold, predictions)
+
+    '''
+    Definitions
+        positive: known
+        negative: unkown
+
+        For example, false positive: predicted positive but it's wrong, so that means you predicted a sammple from unknown class as known
+    '''
+    tpr = {'correct': 0, 'total': 0}
+    fpr = {'correct': 0, 'total': 0}
+    fnr = {'correct': 0, 'total': 0}
+    tnr = {'correct': 0, 'total': 0}
+    id_accuracy = {'correct': 0, 'total': 0}
+
+    for i in range(len(threshold_predictions)):
+
+        # Get the 4 rates
+        if sign(test_y[i]) >= 0: # positives
+            tpr['total'] += 1
+            fnr['total'] += 1
+            if sign(threshold_predictions[i]) >= 0: # true positive
+                tpr['correct'] += 1
+            else: # false negative
+                fnr['correct'] += 1
+        else: # negatives
+            fpr['total'] += 1
+            tnr['total'] += 1
+            if sign(threshold_predictions[i]) >= 0: # false positive
+                fpr['correct'] += 1
+            else: # true negative
+                tnr['correct'] += 1
+        
 
 
+        # Out of known samples that were labeled as known, how many were labeled correctly
+        if sign(threshold_predictions[i]) >= 0 and sign(test_y[i]) >= 0:
+            id_accuracy['total'] += 1
+            if threshold_predictions[i] == test_y[i]:
+                id_accuracy['correct'] += 1
+
+    tpr_list.append(get_accuracy(tpr))
+    fpr_list.append(get_accuracy(fpr))
+    fnr_list.append(get_accuracy(fnr))
+    tnr_list.append(get_accuracy(tnr))
+    id_accuracies.append(get_accuracy(id_accuracy))
+
+
+
+plt.clf()
+plt.plot(threshold_to_test,  id_accuracies)
+plt.savefig('./id.png')
+
+plt.clf()
+plt.plot(threshold_to_test,  tpr_list, label='TPR')
+plt.plot(threshold_to_test,  fpr_list, label='FPR')
+plt.plot(threshold_to_test,  fnr_list, label='FNR')
+plt.plot(threshold_to_test,  tnr_list, label='TNR')
+plt.legend(loc="best")
+plt.savefig('./rates.png')
 
