8ccba628c1ae4620d7466a5a748b9588985bfd2f
jjlee0802cu <jjl2245@columbia.edu>
2023-02-17 15:27:07 -0500

    wrote function to apply static threshold to predictions based on confidence of softmax output

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from sklearn.metrics import DetCurveDisplay
+
 
 # Get Fashion MNIST dataset
+'''
+Original split:
+    Train: 60000 samples
+        0: 6000 samples
+        1: 6000 samples
+        2: 6000 samples
+        3: 6000 samples
+        4: 6000 samples
+        5: 6000 samples
+        6: 6000 samples
+        7: 6000 samples
+        8: 6000 samples
+        9: 6000 samples
+    Test: 10000 samples
+        0: 1000 samples
+        1: 1000 samples
+        2: 1000 samples
+        3: 1000 samples
+        4: 1000 samples
+        5: 1000 samples
+        6: 1000 samples
+        7: 1000 samples
+        8: 1000 samples
+        9: 1000 samples
+'''
 fashion_mnist = keras.datasets.fashion_mnist
 (train_x, train_y), (test_x, test_y) = fashion_mnist.load_data()
 
@@ -13,20 +40,6 @@ test_x = test_x.astype(float)
 test_y = test_y.astype(float)
 
 # Leave out 5 of the classes during training. Classes 5-9 are designated as "unkown"
-"""
-Original split:
-    Train: 60000 samples
-        0~9 have 6000 samples each
-    Test: 10000 samples
-        0~9 have 1000 samples each
-
-Resulting split:
-    Train: 30000 samples
-        0~4 have 6000 samples each
-    Test: 40000 samples
-        0~4 have 1000 samples each
-        -5~-9 has 7000 samples each
-"""
 known_x, known_y = [], []
 unknown_x, unknown_y = [], []
 for i in range(train_x.shape[0]):
@@ -45,11 +58,30 @@ test_y = np.concatenate((test_y, np.array(unknown_y)), axis=0)
 # Change labels of unkown classes to be negative numbers
 for i in range(len(test_y)):
     if test_y[i] >= 5:
-        test_y[i] = -test_y[i] + 4
+        test_y[i] = -1
+"""
+New split:
+    Train: 30000 samples
+        0: 6000 samples
+        1: 6000 samples
+        2: 6000 samples
+        3: 6000 samples
+        4: 6000 samples
+    Test: 40000 samples
+        0: 1000 samples
+        1: 1000 samples
+        2: 1000 samples
+        3: 1000 samples
+        4: 1000 samples
+        -1: 35000 samples
+"""
+print()
 
 
 
 
+
+print("\nTraining model")
 # normalization
 train_x = train_x / 255.0
 test_x = test_x / 255.0
@@ -69,21 +101,38 @@ model.fit(train_x, train_y, epochs=5)
 
 
 
-
+print("\nTesting model")
 # Testing
+
+def sign(num):
+    return 1 if num >= 0 else -1
+
+def apply_threshold(threshold, predictions):
+    threshold_predictions = []
+    for i in range(predictions.shape[0]):
+        argmax = np.argmax(predictions[i])
+        confidence = np.max(predictions[i])
+        if confidence >= threshold:
+            threshold_predictions.append(argmax)
+        else:
+            threshold_predictions.append(-1)
+    return threshold_predictions
+
 predictions = model.predict(test_x)
+threshold_predictions = apply_threshold(0.9, predictions)
 
 
-correct, total = 1, 1
-for i in range(predictions.shape[0]):
-    softmax_output = predictions[i]
+correct = 0
+c,t = 0,0
+for i in range(len(threshold_predictions)):
+    if sign(threshold_predictions[i]) == sign(test_y[i]):
+        correct += 1
+    if sign(threshold_predictions[i]) > 0 and sign(test_y[i]) > 0:
+        t += 1
+        if threshold_predictions[i] == test_y[i]:
+            c += 1
+print(correct/len(threshold_predictions))
+print(c/t)
 
-    y_pred = np.argmax(softmax_output)
-    y_test = test_y[i]
 
-    if y_test >= 0:
-        if y_pred == y_test:
-            correct += 1
-        total += 1
 
-print(correct/total)
