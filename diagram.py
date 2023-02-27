c37f25e6dbfc83f347893fd47cdd011fd7f33b02
jjlee0802cu <jjl2245@columbia.edu>
2023-02-27 11:03:27 -0500

    Add diagrams for paper

---
+++ diagram.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+bars = pd.DataFrame(dict(data=[0.1, 0.05, 0.08, 0.62, 0.1, 0.05]))
+line = pd.DataFrame(dict(data=[0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
+
+fig, ax = plt.subplots()
+bars['data'].plot(kind='bar')
+line['data'].plot(kind='line', color='black', linestyle='dashed')
+
+plt.ylim(0,1)
+plt.xlabel("Known classes")
+plt.ylabel("Softmax confidence")
+plt.savefig('./plots/diagram1.png')
+
+plt.clf()
+bars = pd.DataFrame(dict(data=[0.2, 0.16, 0.05, 0.42, 0.12, 0.05]))
+line = pd.DataFrame(dict(data=[0.5, 0.5, 0.5, 0.5, 0.5, 0.5]))
+
+fig, ax = plt.subplots()
+bars['data'].plot(kind='bar')
+line['data'].plot(kind='line', color='black', linestyle='dashed')
+
+plt.ylim(0,1)
+plt.xlabel("Known classes")
+plt.ylabel("Softmax confidence")
+plt.savefig('./plots/diagram2.png')
+
